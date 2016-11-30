ad61551d547b6005be0d16d228b4b0f83ed5f004
Naomi Joshi <Naomi@Naomis-MacBook-Pro.local>
2016-11-30 02:31:20 -0500

    Adding src folder

---
+++ src/main/java/com/model/WikaModel.java
@@ -0,0 +1,20 @@
+package com.model;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+/**
+ * Created by Naomi on 11/30/16.
+ */
+public class WikaModel {
+    public static void main(String[] args) throws Exception {
+
+        // Declare two numeric attributes
+       
+    }
+
+}
