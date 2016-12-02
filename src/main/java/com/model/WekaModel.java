8075154914f4f2b16d5ea7bd6e2153cfcd63a00e
Sushant Mimani <sushant.mimani@gmail.com>
2016-12-02 07:17:36 -0500

    Classifier

--- src/main/java/com/model/WekaModel.java
+++ src/main/java/com/model/WekaModel.java
@@ -2,6 +2,7 @@ package com.model;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,6 +14,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.trees.J48;
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
@@ -40,7 +42,7 @@ public class WekaModel {
         job0.setJarByClass(WekaModel.class);
         job0.setMapperClass(WekaMapper.class);
         job0.setReducerClass(WekaReducer.class);
-        job0.setOutputKeyClass(Text.class);
+        job0.setOutputKeyClass(IntWritable.class);
         job0.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job0, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job0,
@@ -52,13 +54,32 @@ public class WekaModel {
 
     }
 
-    public static class WekaMapper extends Mapper<Object,Text,Text,Text> {
+    public static class WekaMapper extends Mapper<Object,Text,IntWritable,Text> {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            DataSource source = null;
+        	String val = value.toString();
+        	val = val.substring(1, val.length()-1);
+//        	System.out.println(val);
+        	for(int i = 0; i<10; i++) {
+        		double p = Math.random();
+        		if(p<=0.5) {
+        			context.write(new IntWritable(i), new Text(val));
+        		}
+        	}  
+        }
+    }
+
+
+    public static class WekaReducer extends Reducer<IntWritable,Text,IntWritable,Text> {
+
+        public void reduce(IntWritable key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+        	DataSource source = null;
             Instances instances = null;
             try {
-                source = new DataSource(value.toString());
-                instances = source.getDataSet();
+            	for(Text value : values) {
+            		source = new DataSource(value.toString());
+            		instances = source.getDataSet();
+            	}
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -67,16 +88,49 @@ public class WekaModel {
             // For example, the XRFF format saves the class attribute information as well
             if (instances.classIndex() == -1)
                 instances.setClassIndex(instances.numAttributes() - 1);
+            
+         // Do 10-split cross validation
+    		Instances[][] split = crossValidationSplit(instances,10);
+     
+    		// Separate split into training and testing arrays
+    		Instances[] trainingSplits = split[0];
+    		Instances[] testingSplits = split[1];
+    		
+    		Classifier models = new J48();
+    		FastVector predictions = new FastVector();
+    		for (int i = 0; i < trainingSplits.length; i++) {
+    			Evaluation validation;
+				try {
+					validation = classify(models, trainingSplits[i], testingSplits[i]);
+					predictions.appendElements(validation.predictions());
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+    		System.out.println(models.toString());
+    		context.write(key, new Text(models.toString()));
         }
-    }
-
-
-    public static class WekaReducer extends Reducer<Text,Text,Text,Text> {
-
-        public void reduce(Text key, Iterable<Text> values, Context context)
-                throws IOException, InterruptedException {
-
-        }
+        
+        public static Evaluation classify(Classifier model,
+    			Instances trainingSet, Instances testingSet) throws Exception {
+    		Evaluation evaluation = new Evaluation(trainingSet);
+     
+    		model.buildClassifier(trainingSet);
+    		evaluation.evaluateModel(model, testingSet);
+     
+    		return evaluation;
+    	}
+        public static Instances[][] crossValidationSplit(Instances data, int numberOfFolds) {
+    		Instances[][] split = new Instances[2][numberOfFolds];
+     
+    		for (int i = 0; i < numberOfFolds; i++) {
+    			split[0][i] = data.trainCV(numberOfFolds, i);
+    			split[1][i] = data.testCV(numberOfFolds, i);
+    		}
+     
+    		return split;
+    	}
     }
 
 }
